f3596eb5c461ea4d71b9941df1e22a9ef1cb89ca
arjun <nagarjuna.412@gmail.com>
2013-12-02 12:05:17 +0400

    Minor bugfixes + reformatted boilerplate code to be more readable

--- optparse.bash
+++ optparse.bash
@@ -1,107 +1,153 @@
 # Optparse - a BASH wrapper for getopts
 # @author : nk412 / nagarjuna.412@gmail.com
 
-optparse_usage="" 
+optparse_usage=""
 optparse_contractions=""
 optparse_defaults=""
 optparse_process=""
 optparse_arguments_string=""
 
+# -----------------------------------------------------------------------------------------------------------------------------
 function optparse.throw_error(){
   local message="$1"
-	echo "OPTPARSE: ERROR: $message"
-	exit 1
+        echo "OPTPARSE: ERROR: $message"
+        exit 1
 }
 
+# -----------------------------------------------------------------------------------------------------------------------------
 function optparse.define(){
-  for option_id in $( seq 1 $# ) ; do
-		local option="$( eval "echo \$$option_id")"
-		local key="$( echo $option | awk -F "=" '{print $1}' )";
-		local value="$( echo $option | awk -F "=" '{print $2}' )";
-		
-		#essentials: shortname, longname, description
-		if [ "$key" = "short" ]; then
-			local shortname="$value"
-			if [ ${#shortname} -ne 1 ]; then
-				optparse.throw_error "short name expected to be one character long"
-			fi
-			short="-${shortname}"
-		elif [ "$key" = "long" ]; then
-			local longname="$value"
-			if [ ${#longname} -lt 2 ]; then 
-				optparse.throw_error "long name expected to be atleast one character long"
-			fi
-			long="--${longname}"
-		elif [ "$key" = "desc" ]; then
-			local desc="$value"
-		elif [ "$key" = "default" ]; then
-			local default="$value"
-		elif [ "$key" = "variable" ]; then
-			local variable="$value"
-		elif [ "$key" = "value" ]; then
-			local val="$value"
-		fi
-	done;
+        if [ $# -lt 3 ]; then
+                optparse.throw_error "optparse.define <short> <long> <variable> [<desc>] [<default>] [<value>]"
+        fi
+        for option_id in $( seq 1 $# ) ; do
+                local option="$( eval "echo \$$option_id")"
+                local key="$( echo $option | awk -F "=" '{print $1}' )";
+                local value="$( echo $option | awk -F "=" '{print $2}' )";
 
-  if [ "$variable" = "" ]; then
-		optparse.throw_error "You're supposed to give a variable, dummy! ($short/$long)"
-	fi
-	if [ "$val" = "" ]; then
-		val="\$OPTARG"
-		#optparse.throw_error "Give a value to set, silly! ($short/$long)"
-	fi
-	
-	# build OPTIONS and help
-	optparse_usage="${optparse_usage}\n\t${short} ${long}  :  ${desc}"
-	optparse_contractions="${optparse_contractions}\n\t\t${long})\n\t\t\tparams=\"\$params ${short}\";;"
-	if [ "$default" != "" ]; then
-		optparse_defaults="${optparse_defaults}\n${variable}=${default}"
-	fi
-	optparse_arguments_string="${optparse_arguments_string}${shortname}"
-	if [ "$val" = "\$OPTARG" ]; then
-		optparse_arguments_string="${optparse_arguments_string}:"
-	fi
-	optparse_process="${optparse_process}\n\t${shortname})\n\t\t${variable}=\"$val\";;"
+                #essentials: shortname, longname, description
+                if [ "$key" = "short" ]; then
+                        local shortname="$value"
+                        if [ ${#shortname} -ne 1 ]; then
+                                optparse.throw_error "short name expected to be one character long"
+                        fi
+                        local short="-${shortname}"
+                elif [ "$key" = "long" ]; then
+                        local longname="$value"
+                        if [ ${#longname} -lt 2 ]; then
+                                optparse.throw_error "long name expected to be atleast one character long"
+                        fi
+                        local long="--${longname}"
+                elif [ "$key" = "desc" ]; then
+                        local desc="$value"
+                elif [ "$key" = "default" ]; then
+                        local default="$value"
+                elif [ "$key" = "variable" ]; then
+                        local variable="$value"
+                elif [ "$key" = "value" ]; then
+                        local val="$value"
+                fi
+        done
+
+        if [ "$variable" = "" ]; then
+                optparse.throw_error "You must give a variable for option: ($short/$long)"
+        fi
+
+        if [ "$val" = "" ]; then
+                val="\$OPTARG"
+        fi
+
+        # build OPTIONS and help
+        optparse_usage="${optparse_usage}#NL#TB${short} $(rpad "$long:" 25)  ${desc}"
+        if [ "$default" != "" ]; then
+                optparse_usage="${optparse_usage} [default:$default]"
+        fi
+        optparse_contractions="${optparse_contractions}#NL#TB#TB${long})#NL#TB#TB#TBparams=\"\$params ${short}\";;"
+        if [ "$default" != "" ]; then
+                optparse_defaults="${optparse_defaults}#NL${variable}=${default}"
+        fi
+        optparse_arguments_string="${optparse_arguments_string}${shortname}"
+        if [ "$val" = "\$OPTARG" ]; then
+                optparse_arguments_string="${optparse_arguments_string}:"
+        fi
+        optparse_process="${optparse_process}#NL#TB#TB${shortname})#NL#TB#TB#TB${variable}=\"$val\";;"
 }
 
+# -----------------------------------------------------------------------------------------------------------------------------
 function optparse.build(){
-  local build_file="/tmp/optparse-${RANDOM}.tmp"
+        local build_file="/tmp/optparse-${RANDOM}.tmp"
 
-	# Building getopts header here
- 
-	# Function usage
-	echo -e "function usage(){\n\tcat << EOF\nusage: \$0 [OPTIONS]\n\nOPTIONS:" > $build_file
-	echo -e "$optparse_usage" >> $build_file
-	echo -e "\n\t-? --help  :  usage" >> $build_file
-	echo -e "\nEOF\n}\nif [ \$# -eq 0 ]; then usage ; exit 1 ; fi\n" >> $build_file
-	
-	# Contract long options into short options
-	echo -e "params=\"\"\nfor param in \$*; do\ncase \"\$param\" in\n" >> $build_file
-	echo -e "$optparse_contractions" >> $build_file
-	echo -e "\"-?\"|--help)\nusage\nexit 0;;" >> $build_file
-	echo -e "*)\nif [[ \"$param\" == --* ]]; then" >> $build_file
-	echo -e "echo -ne \"Unrecognized long option: \$param\n\n\"" >> $build_file
-	echo -e "usage; exit 1;" >> $build_file
-	echo -e "fi\nparams=\"\$params \$param\";;\nesac\ndone\neval set -- \"\$params\"" >> $build_file
+        # Building getopts header here
 
-	# Set default variable values
-	echo -e "$optparse_defaults" >> $build_file
-	
-	# Process using getopts
-	echo -e "while getopts \"$optparse_arguments_string\" option; do" >> $build_file
-	echo -e "case \$option in" >> $build_file
-	# Substitute actions for different variables
-	echo -e "$optparse_process" >> $build_file
-	echo -e ":)\necho \"Option - \$OPTARG requires an argument\";exit 1;;" >> $build_file
-	echo -e "*)\nusage ; exit 1;; esac ; done" >> $build_file
-	
-	# Unset global variables
-	unset optparse_usage
-	unset optparse_process
-	unset optparse_arguments_string
-	unset optparse_defaults
-	unset optparse_contractions
-	
-	# Return file name to parent
-	echo "$build_file"	
+        # Function usage
+        cat << EOF > $build_file
+function usage(){
+cat << XXX
+usage: \$0 [OPTIONS]
+
+OPTIONS:
+        $optparse_usage
+
+        -? --help  :  usage
+
+XXX
 }
+
+# Contract long options into short options
+params=""
+while [ \$# -ne 0 ]; do
+        param="\$1"
+        shift
+
+        case "\$param" in
+                $optparse_contractions
+                "-?"|--help)
+                        usage
+                        exit 0;;
+                *)
+                        if [[ "\$param" == --* ]]; then
+                                echo -e "Unrecognized long option: \$param"
+                                usage
+                                exit 1
+                        fi
+                        params="\$params \"\$param\"";;
+        esac
+done
+
+eval set -- "\$params"
+
+# Set default variable values
+$optparse_defaults
+
+# Process using getopts
+while getopts "$optparse_arguments_string" option; do
+        case \$option in
+                # Substitute actions for different variables
+                $optparse_process
+                :)
+                        echo "Option - \$OPTARG requires an argument"
+                        exit 1;;
+                *)
+                        usage
+                        exit 1;;
+        esac
+done
+
+EOF
+
+        local -A o=( ['#NL']='\n' ['#TB']='\t' )
+
+        for i in "${!o[@]}"; do
+                sed -i "s/${i}/${o[$i]}/g" $build_file
+        done
+
+        # Unset global variables
+        unset optparse_usage
+        unset optparse_process
+        unset optparse_arguments_string
+        unset optparse_defaults
+        unset optparse_contractions
+
+        # Return file name to parent
+        echo "$build_file"
+}
+# -----------------------------------------------------------------------------------------------------------------------------
