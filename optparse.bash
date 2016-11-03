35a78d6f78fbfadbd28a9487f3c6fa153f566816
Nagarjuna Kumar <nagarjuna.kumar@duedil.com>
2016-11-03 11:28:33 +0000

    Display license in optparse.bash

--- optparse.bash
+++ optparse.bash
@@ -1,5 +1,27 @@
 # Optparse - a BASH wrapper for getopts
-# @author : nk412 / nagarjuna.412@gmail.com
+# https://github.com/nk412/optparse
+
+# Copyright (c) 2015 Nagarjuna Kumarappan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Author: Nagarjuna Kumarappan <nagarjuna.412@gmail.com>
 
 optparse_usage=""
 optparse_contractions=""
